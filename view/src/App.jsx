c6790cb5bab72eef0a189862c2b2c019e7b2fade
aymen <charfeddineayman760@gmail.com>
2026-04-02 19:32:03 +0100

    Add React frontend for patient inscription page

---
+++ view/src/App.jsx
@@ -0,0 +1,10 @@
+import RegisterPatient from './Components/RegisterPatient';
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterPatient />
+    </div>
+  );
+}
+export default App;
